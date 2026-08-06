51915c5a089899584e8e6f0ba848461ae0fb2137
aden-fahri <adenathoriq@gmail.com>
2026-08-06 14:56:48 +0700

    fix: remove duplicate HTML in laporan component

--- resources/views/components/SatuData/laporan.blade.php
+++ resources/views/components/SatuData/laporan.blade.php
@@ -66,13 +66,6 @@
                                 <span>Unduh PDF</span>
                             </a>
                         </div>
-                                    <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
-                                    <polyline points="7 10 12 15 17 10"></polyline>
-                                    <line x1="12" y1="15" x2="12" y2="3"></line>
-                                </svg>
-                                <span>Unduh PDF</span>
-                            </a>
-                        </div>
                     </div>
                 @empty
                     <div class="laporan-empty-state">
