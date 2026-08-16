94b893ab1ca6c69920366cb151de9d48b747c2cc
aden-fahri <adenathoriq@gmail.com>
2026-08-16 16:38:39 +0700

    refactor(css): organize public CSS files into feature-specific directories

--- resources/views/permohonan.blade.php
+++ resources/views/permohonan.blade.php
@@ -10,7 +10,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700;800&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/permohonan.css') }}?v={{ time() }}">
+    <link rel="stylesheet" href="{{ asset('css/PPID/permohonan.css') }}?v={{ time() }}">
 </head>
 <body>
     @include('layouts.navbar')
